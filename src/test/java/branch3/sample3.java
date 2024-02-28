569a6e0161c3b6c83b22e7939461ddc530f1420b
RakeshSelenium&Testng <RakeshTatukuru97@gmail.com>
2024-02-28 08:57:27 +0530

    sub2 branch code

---
+++ src/test/java/branch3/sample3.java
@@ -0,0 +1,17 @@
+package branch3;
+
+import org.testng.annotations.Test;
+
+public class sample3 {
+
+	@Test
+	public void show()
+	{
+		System.out.println("these is branch 1");
+	}
+	@Test
+	public void display()
+	{
+		System.out.println("these is branch 2");
+	}
+}
